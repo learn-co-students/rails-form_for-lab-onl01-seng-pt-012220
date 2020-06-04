cf7e0f37678315e950dc9153a3253641bd6ca114
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-06-04 12:51:47 -0500

    submit

---
+++ .history/app/controllers/students_controller_20200604081758.rb
@@ -0,0 +1,29 @@
+class StudentsController < ApplicationController
+    def new
+        @student = Student.new
+    end
+    def show
+        @student = set_student
+    end
+    def create
+        @student = Student.create(student_params(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+    def update
+        @student = set_student
+        @student.update(student_params(:first_name))
+        redirect_to student_path(@student)
+    end
+    def edit
+        @student = set_student
+    end
+
+    
+    private
+        def set_student
+            Student.find(params[:id])
+        end
+        def student_params(*args)
+            params.require(:student).permit(*args)
+        end
+end
